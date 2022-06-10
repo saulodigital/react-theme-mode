5d2f70c2eb2a43c2444ca2850f573103da600da8
Saulo <22698083+saulodigital@users.noreply.github.com>
2022-06-10 18:59:48 +0100

    cleaner code, better approach with data attribute

--- src/components/ThemeMode.js
+++ src/components/ThemeMode.js
@@ -2,51 +2,32 @@ import { useEffect, useState } from 'react';
 import './ThemeMode.css';
 
 const ThemeMode = () => {
-  const [ theme, setTheme ] = useState('light');
-  const [ mounted, setMounted ] = useState(false);
-  const body = document.body.classList;
-  const lightTheme = 'light';
-  const darkTheme = 'dark';
+  const [ theme, setTheme ] = useState();
+
+  const localTheme = localStorage.getItem('theme');
+
+  const preferTheme =
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches;
 
   const setMode = mode => {
-    window.localStorage.setItem('theme', mode)
-    setTheme(mode)
-  };
-
-  const switchTheme = () => {
-    if (theme === 'light') {
-      body.replace(lightTheme, darkTheme);
-      setMode('dark');
-    } else {
-      body.replace(darkTheme, lightTheme);
-      setMode('light');
-    }
+    localStorage.setItem('theme', mode);
+    document.documentElement.setAttribute('data-theme', mode);
+    setTheme(mode);
   };
 
   useEffect(() => {
-    const matchMedia = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const localTheme = window.localStorage.getItem('theme');
+    localTheme
+      ? setMode(localTheme)
+      : preferTheme ? setMode('dark') : setMode('light');
+  }, [theme]);
 
-    if (matchMedia && !localTheme) {
-      setMode('dark');
-      body.add(darkTheme);
-    } else {
-      if (localTheme) {
-        setMode(localTheme);
-        body.add(localTheme);
-      } else {
-        setMode('light');
-        body.add(lightTheme);
-      }
-    }
-
-    setMounted(true);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  const switchTheme = () => {
+    theme === 'light' ? setMode('dark') : setMode('light');
+  };
 
   return (
-    [theme, switchTheme, mounted],
-    <button onClick={() => switchTheme()}>
+    <button onClick={switchTheme}>
       <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
         <path d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zm0 13V2a6 6 0 1 1 0 12z"/>
       </svg>
